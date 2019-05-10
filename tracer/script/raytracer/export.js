6c135e94820ebf886a0c92df740f109b59e8ebca
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-05-10 12:14:04 +0300

    minor fix

--- tracer/script/raytracer/export.js
+++ tracer/script/raytracer/export.js
@@ -1,7 +1,7 @@
 export * from './camera.js';
+export * from './features.js';
 export * from './light.js';
 export * from './scene.js';
-export * from './utils.js';
 
 import * as RayTracerMath from './math/export.js';
 export const math = RayTracerMath;
